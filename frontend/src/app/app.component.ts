7b2bc6daff4cadfd42446513679399d42d9ff405
Tomás Travis Alonso Cremnitz <149080929+tomastravis@users.noreply.github.com>
2024-08-04 03:22:26 +0200

    Slowly but surely

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BOATCO2';
+  AppName = 'BOATCO2';
 }
